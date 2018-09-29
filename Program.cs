dc208c30b46fa4d70a18bec102347fe4424563a2
Benjamin Williams <benjamink.williams@gmail.com>
2018-09-29 15:35:08 -0500

    Inital Commit; not working yet

--- Program.cs
+++ Program.cs
@@ -11,5 +11,44 @@ namespace ChromeKiosk
         static void Main(string[] args)
         {
         }
+
+        public CLIOptions ParseOptions(string[] args)
+        {
+            CLIOptions _return = new CLIOptions();
+
+            return _return;
+        }
+
+        public void ShowHelp()
+        {
+            Console.WriteLine("ChromeKiosk" + Environment.NewLine +
+                              "\t-h:                                    Show Help" + Environment.NewLine +
+                              "\t-p:\"C:\\CKSettings\\CKSettings.json\" Path to CKSettings file" + Environment.NewLine
+                              );
+        }
+    }
+
+    public class CLIOptions
+    {
+        bool ShowHelp;
+        string CKSettingsPath;
+    }
+
+    public class CKSettings
+    {
+        List<string> ChromeOptions;
+        List<SiteSettings> SiteSettings;
+    }
+
+    public class SiteSettings
+    {
+        uint TimeoutSeconds;
+        uint TimeoutMinutes;
+
+        uint ViewTimeSeconds;
+        uint ViewTimeMinutes;
+
+        string Uri;
+        string UriSettings;
     }
 }
