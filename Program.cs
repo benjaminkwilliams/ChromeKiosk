81fe95798bb59975126eaddffc786afa5dfe98ae
Benjamin Williams <benjamink.williams@gmail.com>
2018-09-29 15:12:13 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChromeKiosk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
